dd6af3e543c72d6fdbe829321f4b552fbab8180a
MaximilianPorter <mx.porters@gmail.com>
2023-09-01 19:09:51 -0700

    added iframe capability

--- script.js
+++ script.js
@@ -4,12 +4,30 @@
 
 const projectItemArea = document.querySelector(".project-items");
 const pageChannelsArea = document.querySelector(".page-channels");
+const pageLoadingSpinner = document.querySelector(".page-loading-spinner");
 
+const pageContentFrame = document.querySelector(".page-content-iframe");
 const pageContent = document.querySelector(".page-content");
 const sideInfo = document.querySelector(".side-info");
 const mobileHeader = document.querySelector(".mobile-header");
 const toggleSideButton = document.querySelector(".toggle-collapse-sidebar");
 const mobileHeaderTitle = document.querySelector(".mobile-header-channel-name");
+
+let urlParams = new URLSearchParams(window.location.search);
+let urlProjectId = urlParams.get("project") ?? 0;
+let urlChannelName = urlParams.get("channel") ?? "";
+console.log(urlProjectId, urlChannelName);
+
+const css = `
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@200;400;500;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="${window.origin}/general.css" />
+    <link rel="stylesheet" href="${window.origin}/style.css" />
+`;
 
 let hashSvg = `
 <svg
@@ -37,9 +55,13 @@ let hashSvg = `
 
 let sideBarOpen = false;
 let projects = [];
+let currentSelectedProject = null;
 
-AddEventListeners();
+// log which script is running
+console.log(`script.js is running`);
+
 await PopulateProjects();
+AddEventListeners();
 
 async function PopulateProjects() {
   const getProjects = await fetch("./projects.json");
@@ -50,27 +72,27 @@ async function PopulateProjects() {
     const projectImageSrc = project.image;
 
     const projectItemMarkup = `
-    <div class="project-item" data-project-name="${projectName}" data-id="${i}">
-      <div class="project-image-container">
-        <img
-          src="${projectImageSrc}"
-          alt="image for ${projectName}"
-        />
-      </div>
-      <div class="project-item-hover-element"></div>
-      <div class="project-item-name-hover">
-        <p>${projectName}</p>
-      </div>
+  <div class="project-item" data-project-name="${projectName}" data-id="${i}">
+    <div class="project-image-container">
+      <img
+        src="${projectImageSrc}"
+        alt="image for ${projectName}"
+      />
     </div>
-  `;
+    <div class="project-item-hover-element"></div>
+    <div class="project-item-name-hover">
+      <p>${projectName}</p>
+    </div>
+  </div>
+`;
     projectItemArea.insertAdjacentHTML("beforeend", projectItemMarkup);
   });
 
   // click on first project
-  ClickOnProjectItem(0);
+  ClickOnProjectItem(urlProjectId);
 }
 
-function ClickOnProjectItem(index) {
+function ClickOnProjectItem(projectId) {
   // remove selected class from all projects
   const allProjectItems = document.querySelectorAll(".project-item");
   allProjectItems.forEach((projectItem) => {
@@ -78,36 +100,97 @@ function ClickOnProjectItem(index) {
   });
 
   // add selected class to clicked project
-  const projectItem = document.querySelector(`[data-id="${index}"]`);
+  const projectItem = document.querySelector(`[data-id="${projectId}"]`);
   projectItem.classList.add("project-item-selected");
 
-  pageChannelsArea.innerHTML = "";
-  const project = projects[index];
+  currentSelectedProject = projects[projectId];
 
-  project.channels.forEach((channel, i) => {
-    const channelName = channel.name;
+  // set url params
+  urlParams.set("project", projectId);
+  window.history.replaceState({}, "", `${location.pathname}?${urlParams}`);
+  urlProjectId = projectId;
 
-    const markup = `
-    <div class="page-channel" data-channel-name="${channelName}" 
-    data-channel-id="${i}"
-    data-project-id="${index}">
-      ${hashSvg}
-      <p class="channel-name">${channelName}</p>
-    </div>
-    `;
-
-    pageChannelsArea.insertAdjacentHTML("beforeend", markup);
-  });
-
-  // click on first channel
-  const firstChannel = pageChannelsArea.querySelector(".page-channel");
-  ClickOnChannel(firstChannel, 0);
+  PopulateChannels(projectId);
 }
 
-function ClickOnChannel(pageChannel) {
-  // show loading animation
-  pageContent.innerHTML = `<div class="lds-ring"><div></div><div></div><div></div><div></div></div>`;
+async function PopulateChannels(projectId) {
+  const project = projects[projectId];
+  const channelType = project.channelType;
 
+  pageChannelsArea.innerHTML = "";
+  if (channelType === "single-page") {
+    await PopulateChannels_SinglePage(projectId);
+  } else if (channelType === "multi-page") {
+    await PopulateChannels_MultiPage(projectId);
+  }
+
+  // click on first channel
+  const foundChannel = document.querySelector(
+    `[data-channel-name="${urlChannelName}"]`
+  );
+  const firstChannel = document.querySelector(".page-channel");
+  ClickOnChannel(foundChannel || firstChannel);
+}
+
+async function PopulateChannels_SinglePage(projectId) {
+  const project = projects[projectId];
+  const projectSourcePage = project.source;
+  if (!projectSourcePage) return;
+
+  const content = await SetPageContentFromSource(projectSourcePage);
+  const singlePageLinks = FindLinksInContent(content);
+  singlePageLinks.forEach((link) => {
+    const div = AddChannelDiv(
+      projectId,
+      {
+        name: link,
+      },
+      0,
+      link
+    );
+  });
+}
+async function PopulateChannels_MultiPage(projectId) {
+  const project = projects[projectId];
+  project.channels.forEach((channel, i) => {
+    AddChannelDiv(projectId, channel, i);
+  });
+}
+
+function AddChannelDiv(projectId, channel, i = 0, linkName = "") {
+  const { name: channelName } = channel;
+
+  const markup = `
+    <a 
+    href="#${linkName}"
+    class="page-channel" 
+    data-channel-name="${channelName}" 
+    data-channel-id="${i}"
+    data-project-id="${projectId}">
+      ${hashSvg}
+      <p class="channel-name">${channelName}</p>
+    </a>
+    `;
+
+  pageChannelsArea.insertAdjacentHTML("beforeend", markup);
+
+  return pageChannelsArea.lastElementChild;
+}
+
+function FindLinksInContent(content) {
+  // only find href links that start with # and are followed by a word
+  const links = content.match(/(?<=href="#)\w+/g);
+
+  if (!links) {
+    console.error(`Project has no links. Please add links to the project.`);
+    return null;
+  }
+
+  // return links without duplicates
+  return [...new Set(links)];
+}
+
+async function ClickOnChannel(pageChannel) {
   // remove selected class from all channels
   const allPageChannels = document.querySelectorAll(".page-channel");
   allPageChannels.forEach((channel) => {
@@ -117,28 +200,75 @@ function ClickOnChannel(pageChannel) {
   // add selected class to clicked channel
   pageChannel.classList.add("page-channel-selected");
 
-  mobileHeaderTitle.textContent = pageChannel.dataset.channelName;
+  if (!currentSelectedProject)
+    currentSelectedProject = projects[pageChannel.dataset.projectId];
 
-  const channelContent =
-    projects[pageChannel.dataset.projectId].channels[
-      pageChannel.dataset.channelId
-    ].content;
-
-  if (channelContent === "") {
-    const contentSrc =
-      projects[pageChannel.dataset.projectId].channels[
-        pageChannel.dataset.channelId
-      ].source;
-
-    // read file with the name of the source
-    fetch(contentSrc)
-      .then((response) => response.text())
-      .then((data) => {
-        pageContent.innerHTML = data;
+  if (currentSelectedProject.channelType === "single-page") {
+    // scroll page content to .page-channel link
+    const link = pageChannel.getAttribute("href");
+    const linkElement =
+      pageContentFrame.contentWindow.document.body.querySelector(`${link}`);
+    if (linkElement) {
+      pageContentFrame.contentWindow.scrollTo({
+        top: linkElement.offsetTop,
+        behavior: "smooth",
       });
-  } else {
-    pageContent.innerHTML = channelContent;
+    } else {
+      console.error(`Link ${link} does not exist.`);
+    }
+  } else if (currentSelectedProject.channelType === "multi-page") {
+    const project = projects[pageChannel.dataset.projectId];
+    const channelId = pageChannel.dataset.channelId;
+    const channelContent = project.channels[channelId].content;
+
+    mobileHeaderTitle.textContent = pageChannel.dataset.channelName;
+
+    if (channelContent === "") {
+      const contentSrc = project.channels[channelId].source;
+      SetPageContentFromSource(contentSrc);
+    } else {
+      ChangePageContent(channelContent);
+    }
   }
+
+  // set url channel params
+  urlParams.set("channel", pageChannel.dataset.channelName);
+  window.history.replaceState({}, "", `${location.pathname}?${urlParams}`);
+  urlChannelName = pageChannel.dataset.channelName;
+}
+
+async function SetPageContentFromSource(source) {
+  pageLoadingSpinner.classList.remove("hidden"); // show loading spinner
+
+  pageContentFrame.src = source;
+  const data = await new Promise((resolve) => {
+    pageContentFrame.addEventListener("load", () => {
+      const data = pageContentFrame.contentWindow.document.body.innerHTML;
+      resolve(data);
+      pageLoadingSpinner.classList.add("hidden"); // hide loading spinner
+    });
+  });
+  return data;
+}
+
+function ChangePageContent(content) {
+  pageLoadingSpinner.classList.remove("hidden"); // show loading spinner
+  // create src link for content
+  // Create a blob containing the HTML content
+  const blob = new Blob([css + content], { type: "text/html" });
+
+  // Generate a URL for the blob
+  const blobURL = URL.createObjectURL(blob);
+
+  // Set the iframe src to the blob URL
+  pageContentFrame.src = blobURL;
+  pageContentFrame.onload = () => {
+    pageLoadingSpinner.classList.add("hidden"); // hide loading spinner
+  };
+
+  // Revoke the blob URL later when no longer needed
+  // (e.g. on unload)
+  URL.revokeObjectURL(blobURL);
 }
 
 function AddEventListeners() {
@@ -153,6 +283,7 @@ function AddEventListeners() {
 
   // clicking on a channel
   pageChannelsArea.addEventListener("click", (e) => {
+    e.preventDefault();
     const pageChannel = e.target.closest(".page-channel");
     if (!pageChannel) return;
 
